acb01b3a2a4d26ba909b11b483ca2ce56da3dabd
i-saw-sparks <andresmolinaceti@gmail.com>
2020-11-13 22:18:14 -0600

    Node definition

--- List.h
+++ List.h
@@ -5,10 +5,25 @@
 #ifndef LSLLSE_LIST_H
 #define LSLLSE_LIST_H
 
+#include <exception>
+#include <string>
+
 template <typename T>
 class List {
 private:
+    class Node{
+    private:
+        T data;
+        Node* next;
+    public:
+        Node();
+        Node(const T&);
+        T getData() const;
+        Node* getNext() const;
 
+        void setData(const T&);
+        void setNext(Node*);
+    };
 
 public:
     class Exception:public std::exception{
